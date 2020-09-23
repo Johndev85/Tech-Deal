d8bdecb6cf06965abe67c84ab1f46cf196c7dcf6
John Botero <jhondark@gmail.com>
2020-09-23 01:50:47 -0500

    header done

---
+++ components/Header.jsx
@@ -0,0 +1,24 @@
+import styles from "../components/header.module.scss"
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <img
+                className={styles.header__logo}
+                src="https://i.imgur.com/f83h2PS.png"
+                alt="logo-tech-deal"
+            />
+
+            <nav className={styles.header__menu}>
+                <a href="">Categories</a>
+                <a href="">Favorites</a>
+                <img
+                    className={styles.header__menu__userImg}
+                    src="/images/user.svg"
+                    alt="user-icon"
+                />
+                <a href="">User</a>
+            </nav>
+        </header>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <section className="searchContainer">
+                <h1>The Best Tech Offers For you</h1>
+                <h2>Online price and offers comparator</h2>
+            </section>
+        </>
+    )
+}
