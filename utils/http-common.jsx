a549975dccd918deefa547726a0c62a96d91164d
Enriketo <55071663+Enriketo@users.noreply.github.com>
2020-10-04 12:26:52 -0500

    Update http-common.jsx

--- utils/http-common.jsx
+++ utils/http-common.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'http://3.130.55.84:4000/',
     headers:{
         'Content-type': 'application/json'
     }
